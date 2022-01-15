59ea3822896731f5000a463077f6a181d27da5ed
전량원 <rise@jeonlyang-won-ui-MacBookPro.local>
2022-01-15 23:10:28 +0900

    finish

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,6 +7,7 @@ interface ButtonProps {
   col: number;
   state: CellState;
   value: CellValue;
+  red?: boolean;
   onClick(rowParam: number, colParam: number): (...args: any[]) => void;
   onContext(rowParam: number, colParam: number): (...args: any[]) => void;
 }
@@ -18,6 +19,7 @@ const Button: React.FC<ButtonProps> = ({
   state,
   value,
   onContext,
+  red,
 }) => {
   const renderContent = (): React.ReactNode => {
     if (state === CellState.visible) {
@@ -45,7 +47,7 @@ const Button: React.FC<ButtonProps> = ({
     <div
       className={`Button ${
         state === CellState.visible ? "visible" : ""
-      } value-${value}`}
+      } value-${value} ${red ? "red" : ""}`}
       onClick={onClick(row, col)}
       onContextMenu={onContext(row, col)}
     >
